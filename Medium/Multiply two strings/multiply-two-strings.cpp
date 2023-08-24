0582528a757e71186da5271b79515f10ba8079b1
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-08-24 11:41:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Multiply two strings/multiply-two-strings.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    /*You are required to complete below function */
+    string multiplyStrings(string s1, string s2) {
+        if(s1 == "0" || s2 == "0"){
+           return "0";
+       }
+       int flag = 0;
+        if(s1[0]=='-'){
+            flag++;
+            s1 = s1.substr(1,s1.size()-1);
+        }
+          
+        if(s2[0]=='-'){
+            flag++;
+            s2 = s2.substr(1,s2.size()-1);
+        }
+       vector<int> ans(s1.size()+s2.size(), 0);
+       for(int i=s1.size()-1 ; i>=0 ; i--){
+           for(int j=s2.size()-1 ; j>=0 ; j--){
+               ans[i+j+1] += (s1[i]-'0') * (s2[j] - '0');
+               ans[i+j] += ans[i+j+1]/10;
+               ans[i+j+1] %= 10;
+           }
+       }
+       int i=0;
+       while(i<ans.size() && ans[i] == 0){
+           i++;
+       }
+       string sol = "";
+       for(int j=i ; j<ans.size() ; j++){
+           sol.push_back(ans[j] + '0');
+       }
+       if(flag == 1){
+           sol = "-" + sol;
+       }
+       return sol;
+    }
+
+};
+
+
+
+//{ Driver Code Starts.
+ 
+int main() {
+     
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	string a;
+    	string b;
+    	cin>>a>>b;
+    	Solution obj;
+    	cout<<obj.multiplyStrings(a,b)<<endl;
+    }
+     
+}
+// } Driver Code Ends
