773c915585a31defbf791980050c9e2a0b1293b6
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-09-17 23:21:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Print first n Fibonacci Numbers/print-first-n-fibonacci-numbers.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+   
+    vector<long long> printFibb(int n) 
+    {
+       vector<long long>ans(n,1);
+        for(int i=2;i<n;i++)ans[i]=ans[i-1]+ans[i-2];
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
