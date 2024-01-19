aa1f73c10122951356bf4b3a32946571abc191e7
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-01-19 11:39:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	
+    vector <int> dijkstra(int v, vector<vector<int>> adj[], int s)
+    {
+        vector<int>dist(v,INT_MAX);
+        queue<int>q;
+        
+        dist[s]=0;
+        q.push(s);
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            
+            for(auto it : adj[node]){
+                int nxtnd=it[0];
+                int nxtdis=it[1];
+                
+                if(dist[node]+nxtdis<dist[nxtnd]){
+                    dist[nxtnd]=dist[node]+nxtdis;
+                    q.push(nxtnd);
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
