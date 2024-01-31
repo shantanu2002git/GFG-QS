a5b502dd5795b1492a0873e32887d8734dc0b467
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-02-01 00:14:46 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum number of jumps/minimum-number-of-jumps.cpp
+++ Medium/Minimum number of jumps/minimum-number-of-jumps.cpp
@@ -6,56 +6,27 @@ using namespace std;
 // } Driver Code Ends
 // Function to return minimum number of jumps to end of array
 
-/*
 class Solution{
   public:
-  int cal(int idx, int n, int arr[], vector<int>&dp){
-      if(idx==n){
-          return 0;
-      }
-      if(dp[idx]!=-1){
-          return dp[idx];
-      }
-      int jmp=1e9;
-      for(int i=arr[idx]; i>=1; i--){
-          if(idx+i<=n){
-              jmp=min(jmp,1+cal(idx+i,n,arr,dp));
-          }
-      }
-      return dp[idx]=jmp;
+  int call(int arr[], int n){
+     int mx=0,curr=0,c=0;
+     for(int i=0; i<n-1; i++){
+         mx=max(mx,arr[i]+i);
+         if(i==curr){
+             c++;
+             curr=mx;
+         }
+     }
+     
+     if(curr>=n-1){
+         return c;
+     }
+     return -1;
   }
     int minJumps(int arr[], int n){
-        // Your code here
-        vector<int>dp(n,-1);
-        return cal(0,n-1,arr,dp)==1e9 ?-1:cal(0,n-1,arr,dp);// T.c O(n) S.C O(n*n)
+       return call(arr,n);
     }
 };
-
-*/
-// Function to return minimum number of jumps to end of array
-
-class Solution{
-  public:
-    int minJumps(int arr[], int n){
-        int furthest=0; 
-        int end=0;
-        int jump=0;
-       for(int ind=0;ind<n-1;ind++)
-        {
-            furthest=max(furthest,arr[ind]+ind);
-            if(end==ind)
-            {
-                jump++;
-                if(end==furthest){
-                return -1;
-                }
-                end=furthest;
-            }
-        }
-        return jump;
-    }
-};
-
 
 
 //{ Driver Code Starts.
