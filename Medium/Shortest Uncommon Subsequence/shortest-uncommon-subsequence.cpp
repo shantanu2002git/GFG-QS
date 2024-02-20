049f4065c3403cc5e3465c9ceb23e8d691013446
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-02-20 23:33:21 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shortest Uncommon Subsequence/shortest-uncommon-subsequence.cpp
+++ Medium/Shortest Uncommon Subsequence/shortest-uncommon-subsequence.cpp
@@ -3,46 +3,62 @@
 using namespace std;
 
 // } Driver Code Ends
+
+#include <vector>
+#include <string>
+using namespace std;
+
 class Solution {
-  public:
-   int findShortest(vector<vector<int>> &dp, string &S, string &T, int n, int m, int i, int j){
-        //Why this?
-        //This is because if we reach end of the string it means -> we were not able to find a subsequence which was not common
-        if(i >= n){
-            return 501;
+public:
+    
+    int findShortest(vector<vector<int>> &dp, string &S, string &T, int n, int m, int i, int j) {
+        // If we reach the end of S, it means we were not able to find a subsequence which was not common
+        if (i >= n) {
+            return 1e9; // Return a large value as a placeholder
         }
-        //Now if i is not reached at end yet but j is reached we can definitely say that if now I take ith character in our subsequence, it will definitely create an uncommon subsequence
-        if(j >= m){
-            return 1;
+        // If we reach the end of T but not of S, then any remaining characters in S will be uncommon
+        if (j >= m) {
+            return 1; // We can add any remaining characters of S to make an uncommon subsequence
         }
-        //if we already know that for particular i, j position, what is the minimum length of the uncommon subsequence then return it
-        if(dp[i][j] != -1){
+     
+       if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        //Suppose any of the above condition is not true, we will try to find current ith character in T string starting from j,
-        int k = j;
-        for(; k < m; k++){
-            //if we find the character, it gives us two options -> either take that character in the uncommon subsequence and increase j to k + 1 and check to next character or don't add to subsequence and check for next character
-            if(S[i] == T[k]){
-                break;
+        
+        // Find the position of the character S[i] in T starting from index j
+        int k;
+        for (k = j; k < m; k++) {
+            if (S[i] == T[k]) {
+                break; // Found a matching character
             }
         }
-        //or in case we did not find the ith character of S in T, then it's sure that we will get the uncommon subsequence by adding that character to our current ans
-        if(k == m){
-            return 1;
+        
+        // If we didn't find the character S[i] in T, then adding it to the subsequence will make it uncommon
+        if (k == m) {
+            return 1; // Adding S[i] to the subsequence makes it uncommon
         }
+        
+        // Two options:
+        // 1. Do not include S[i] in the subsequence, move to the next character in S
+        // 2. Include S[i] in the subsequence, move to the next character in both S and T
         return dp[i][j] = min(findShortest(dp, S, T, n, m, i + 1, j), 1 + findShortest(dp, S, T, n, m, i + 1, k + 1));
     }
   
-  
+    // Main function to find the shortest uncommon subsequence
     int shortestUnSub(string S, string T) {
         int n = S.size();
         int m = T.size();
-        vector<vector<int>> dp (n + 1, vector<int> (m + 1, -1));
+      
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        
         int ans = findShortest(dp, S, T, n, m, 0, 0);
-        return ans >= 501 ? -1 : ans;
+        // If the result is greater than or equal to 501, return -1 as no uncommon subsequence found
+        return ans >= 1e9 ? -1 : ans;
     }
 };
+
+
+
 
 //{ Driver Code Starts.
 int main() {
