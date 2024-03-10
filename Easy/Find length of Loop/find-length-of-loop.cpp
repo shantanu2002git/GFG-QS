874da514ccdd4e3f2d4bfd1ff70332edeca1f213
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-03-11 00:31:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find length of Loop/find-length-of-loop.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+
+int go(struct Node* slow) {
+    int c = 1;
+    struct Node* first = slow->next;
+    while (first != slow) {
+        c++;
+        first = first->next;
+    }
+    return c;
+}
+
+int countNodesinLoop(struct Node* head) {
+    struct Node* first = head, *slow = head;
+    int c = 0;
+    while (first != NULL && first->next != NULL) {
+        first = first->next->next;
+        slow = slow->next;
+        if (first == slow) {
+            return go(slow);
+        }
+    }
+    return c;
+}
