138eb7078313574190aa970735bdd97b229e0b06
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-11-08 10:47:01 +0530

    Time: 49 ms (34.71%), Space: 29.6 MB (55.12%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string compressedString(string w) {
-        int n = w.size(), i = 1, c = 1;
+        int n = w.size(), i = 0, c = 0;
         string res = "";
-        while (i < n)
-        {
-
-            while (w[i - 1] == w[i] && i < n && c < 9)
-            {
-                c++;
+        while (i < n) {
+            char ci = w[i];
+            i = i + 1;
+            c = c + 1;
+            while (ci == w[i] && i < n && c <9) {
                 i++;
+                c++;
             }
             res.append(to_string(c));
-            res.push_back(w[i - 1]);
-            c = 1;
-            i++;
+            res.push_back(ci);
+            c = 0;
         }
-        return res; 
+        return res;
     }
 };
