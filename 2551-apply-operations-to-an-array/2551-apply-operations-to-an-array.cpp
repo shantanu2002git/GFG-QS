682d060a8c3bb93718bef62366855fc326ce86e0
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-11-25 14:31:26 +0530

    Time: 0 ms (100%), Space: 11.3 MB (99.08%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        
+       
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        
+        
+        int pos = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[pos++] = nums[i];
+            }
+        }
+        
+       
+        for (int i = pos; i < n; i++) {
+            nums[i] = 0;
+        }
+
+        return nums;
+    }
+};
