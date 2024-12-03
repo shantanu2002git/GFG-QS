e20d0dca3ba2180bd5073b52f27e50df86acb5b6
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-12-03 10:43:32 +0530

    Time: 0 ms (100%), Space: 13.9 MB (46.97%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,25 +1,15 @@
-
-class Solution
-{
+class Solution {
 public:
-    bool checkIfExist(vector<int> &arr)
-    {
+    bool checkIfExist(vector<int>& arr) {
         unordered_map<int, int> mp;
-
-        for (int num : arr)
-        {
-            if (mp.find(num * 2) != mp.end())
-            {
-                return true;
-            }
-           if (num % 2 == 0 && mp.find(num / 2) != mp.end())
-            {
+        for (int num : arr) {
+            if ((mp.find(num * 2) != mp.end()) ||
+                (num % 2 == 0 && mp.find(num / 2) != mp.end())) {
                 return true;
             }
 
             mp[num]++;
         }
-
         return false;
     }
 };
