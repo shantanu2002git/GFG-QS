d691da3bbce3b998ebf6a91ec25bf65402a61479
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-12-08 10:02:56 +0530

    Time: 64 ms (79.55%), Space: 117.5 MB (97.86%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -3,67 +3,26 @@ class Solution
 public:
     int maxCount(vector<int> &bann, int n, int target)
     {
-        set<int> st;
-        for (int i : bann)
-        {
-            st.insert(i);
-        }
-        vector<int> nwarr;
+        sort(bann.begin(), bann.end()); 
+        int res = 0;                
+        int currentSum = 0;          
+        
         for (int i = 1; i <= n; i++)
         {
-            if (st.count(i) == 0)
+           
+            if (binary_search(bann.begin(), bann.end(), i))
+                continue; // Skip this number if it is banned
+            
+            if (currentSum + i <= target)
             {
-                nwarr.push_back(i);
+                res++;         
+                currentSum += i; 
+            }
+            else
+            {
+                break; 
             }
         }
-        int sum = 0, res = 0;
-        for (auto it : nwarr)
-        {
-            if (target - it >= 0)
-            {
-                res++;
-                cout << it << " ";
-                target = target - it;
-            }
-        }
-        /*
-        int m = bann.size();
-        for (int i = 1; i <= n; i++)
-        {
-            int fn = i;
-            int l = 0, r = m, flg = 0;
-            while (l < r)
-            {
-                int md = (l + r) / 2;
-                if (bann[md] == fn)
-                {
-                    cout<<fn<<"---";
-                    flg = 1;
-                    //break;
-                }
-                if (bann[md] > fn)
-                {
-                    r = md - 1;
-                }
-                else if(bann[md] < fn)
-                {
-                    l = md + 1;
-                }
-            }
-            if (!flg)
-            {
-                if (target - fn >= 0)
-                {
-                    res++;
-                  //  cout << fn << " ";
-                    target = target - fn;
-                }
-            }
-        }*/
-        return res;
+        return res; 
     }
 };
-
-/*
-if the question ask "maximum number of integers you can choose to get Target Sum..."
-*/
