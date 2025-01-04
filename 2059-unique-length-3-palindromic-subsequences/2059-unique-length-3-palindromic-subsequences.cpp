090efe0fbd15c60869bfc265ae39f1f135acfc90
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-01-04 22:41:49 +0530

    Time: 508 ms (27%), Space: 16.3 MB (37.12%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         int n = s.size();
-        vector<int> first(26, -1), last(26, -1);
-        unordered_set<string> unique_palindromes;
+        vector<int> lefcount(26, 0), rightcount(26, 0);
+        map<char, int> index;
+        int res = 0;
 
-        // Record the first and last occurrence of each character
+       
+        for (int i = n - 1; i >= 0; i--) {
+            if (index.find(s[i]) == index.end()) {
+                index[s[i]] = i;
+            }
+        }
+
+        // Traverse the string and count unique palindromic subsequences
         for (int i = 0; i < n; i++) {
-            if (first[s[i] - 'a'] == -1) {
-                first[s[i] - 'a'] = i;
-            }
-            last[s[i] - 'a'] = i;
-        }
-
-        // Check for palindromic subsequences
-        for (int i = 0; i < 26; i++) {
-            if (first[i] != -1 && last[i] != -1 && first[i] != last[i]) {
-                unordered_set<char> middle_chars;
-                for (int j = first[i] + 1; j < last[i]; j++) {
-                    middle_chars.insert(s[j]);
-                }
-                for (char c : middle_chars) {
-                    unique_palindromes.insert(string(1, 'a' + i) + c + string(1, 'a' + i));
+            set<char> st;
+            int ci = s[i] - 'a';
+            if (lefcount[ci] == 0) {
+                for (int j = i + 1; j < index[s[i]]; j++) {
+                    st.insert(s[j]);
                 }
             }
+            lefcount[ci]=1;//already cover
+            res += st.size();
         }
 
-        return unique_palindromes.size();
+        return res;
     }
 };
