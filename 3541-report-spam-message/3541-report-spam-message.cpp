d7137eb0a2ce88e53d4e71a7b1fa21b9a7bf16fa
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-02-18 22:53:17 +0530

    Time: 521 ms (21.99%), Space: 300 MB (32.99%) - LeetHub

---
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        map<string, int>mp;
+        for(auto it : bannedWords){
+            mp[it]++;
+        }
+        int c=0;
+        for(auto it : message){
+            // if(c>=2){
+            //     return true;
+            // }
+            if(mp.find(it)!=mp.end()){
+                c++;
+            }
+             if(c>=2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
