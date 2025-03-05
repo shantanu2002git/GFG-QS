daaa939ea9bd910ddb9a0b75c4f9b3071421d768
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-05 14:29:18 +0530

    Time: 23 ms (34.88%), Space: 171.8 MB (49.92%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
@@ -25,8 +25,11 @@ public:
         while (revHead) {
             if (revHead->val >= maxval) {
                 going->next = new ListNode(revHead->val) ;
+                 // insert the value in new linked list
                 maxval = revHead->val;
+                // update the max-value by present max-value
                 going = going->next;
+                // pointing next to the new on going list
             }
             revHead = revHead->next;
         }
