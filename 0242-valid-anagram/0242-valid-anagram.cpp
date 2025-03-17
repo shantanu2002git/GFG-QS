7f65f5a1b8d56e4af54c423978ab786770a787dd
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-17 22:54:32 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
         int n = s.size(), m = t.size();
         if (n != m) {
             return 0;
         }
+        /* nlogn*/
+        /*
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
         for (int i = 0; i < n; i++) {
             if (s[i] != t[i]) {
+                return 0;
+            }
+        }
+        return 1;
+        */
+
+        vector<int> mps(26, 0);
+        for (auto i : s) {
+            mps[i - 'a']++;
+        }
+        for (auto j : t) {
+            mps[j - 'a']--;
+        }
+        for (int it : mps) {
+            if (it < 0) {
                 return 0;
             }
         }
