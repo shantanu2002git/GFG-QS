f3bee9353c00757072dd30db46cb7521ec33a0d6
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-17 23:02:31 +0530

    Time: 2 ms (78.28%), Space: 15.1 MB (11.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> mps;
         for(int i=0; i<nums.size(); i++){
+            if(mps.find(target-nums[i])!=mps.end() && i!=mps[target-nums[i]]){
+                return {i, mps[target-nums[i]]};
+            }
             mps[nums[i]]=i;
 
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(mps.find(target-nums[i])!=mps.end() && i!=mps[target-nums[i]]){
-                return {i, mps[target-nums[i]]};
-            }
-        }
+      
         return {};
     }
 };
