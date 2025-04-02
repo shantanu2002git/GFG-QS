4a4159586ef3d5030b88b0ebba443cbb122304bd
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-04-02 15:56:58 +0530

    Time: 0 ms (100%), Space: 12.4 MB (85.87%) - LeetHub

--- 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
+++ 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
@@ -9,10 +9,13 @@ public:
         for(int i=0; i<n; i++){
             q.push(i);
         }
-
+/*
+[17,13,11,2,3,5,7]
+[2 3 5 7 11 13 17 ]
+*/
         for(int j=0; j<n; j++){
-            int idx=q.front(); q.pop(); //0
-              res[idx]=deck[j];
+            int idx=q.front(); q.pop(); //0 2 4
+              res[idx]=deck[j];// 2 3 5
               if(!q.empty()){
                 int nxt=q.front(); 
                 q.pop();
