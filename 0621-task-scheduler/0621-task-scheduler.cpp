905df0287dcd657ede8e6e9737802157ce6729b7
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-04-10 10:02:53 +0530

    Time: 0 ms (100%), Space: 38.2 MB (84.07%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> freq;
-        for (char task : tasks) freq[task]++;
-
-        priority_queue<int> pq;
-        for (auto [ch, count] : freq) pq.push(count);
-
-        int time = 0;
-        while (!pq.empty()) {
-            vector<int> temp;
-            int cycle = n + 1;
-
-            // Process up to (n + 1) tasks in a cycle
-            while (cycle-- && !pq.empty()) {
-                int cnt = pq.top(); pq.pop();
-                if (--cnt > 0) temp.push_back(cnt);
-                time++;
-            }
-
-            // Push remaining back to heap
-            for (int cnt : temp) pq.push(cnt);
-
-            // If tasks left, we were idle
-            if (!pq.empty()) time += cycle + 1;
+        vector<int> freq(26, 0);
+        for (char task : tasks) {
+            freq[task - 'A']++;
         }
 
-        return time;
+        int maxFreq = *max_element(freq.begin(), freq.end());
+        int maxCount = count(freq.begin(), freq.end(), maxFreq);
+
+        int partCount = maxFreq - 1;
+        int partLength = n + 1;
+        int emptySlots = partCount * partLength + maxCount;
+
+        return max((int)tasks.size(), emptySlots);
     }
 };
