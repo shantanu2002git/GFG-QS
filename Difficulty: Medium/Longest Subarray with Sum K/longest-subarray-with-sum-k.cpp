732776a5bc67399b767213229a469b1f6dc3a7ac
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-11-29 19:32:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // code here
+         unordered_map<int, int> mp;
+    int res = 0;
+    int prefSum = 0;
+
+    for (int i = 0; i < arr.size(); ++i) {
+        prefSum += arr[i];
+
+        // Check if the entire prefix sums to k
+        if (prefSum == k) 
+            res = i + 1;
+
+        // If prefixSum - k exists in the map then there exist such 
+      	// subarray from (index of previous prefix + 1) to i.
+        else if (mp.find(prefSum - k) != mp.end()) 
+            res = max(res, i - mp[prefSum - k]);
+
+        // Store only first occurrence index of prefSum
+      	if (mp.find(prefSum) == mp.end())
+            mp[prefSum] = i;
+    }
+
+    return res;
+    }
+};
